1dbcbd8c0161a1130cb2fa48b1139d321d36d076
Ilkin Bayramli <43158991+ibayramli2001@users.noreply.github.com>
2019-06-11 19:39:47 +0400

    Update k_nearest_neighborhood.py

--- Classifer/k_nearest_neighborhood.py
+++ Classifer/k_nearest_neighborhood.py
@@ -32,7 +32,7 @@ get_ipython().run_line_magic('autoreload', '2')
 
 # %%
 # Load the raw CIFAR-10 data.
-cifar10_dir = 'Datasets/cifar-10-batches-py'
+cifar10_dir = 'cifar-10-batches-py'
 X_train, y_train, X_test, y_test = load_CIFAR10(cifar10_dir)
 
 # %%
